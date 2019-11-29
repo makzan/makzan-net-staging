16247003d128ef786121f78023d418d2b477ef43
Thomas Seng Hin Mak <mak@makzan.net>
2019-11-29 23:04:00 +0800

    Init.

---
+++ wp-atom.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://makzan.blog/feed/atom/">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://makzan.blog/feed/atom/";
+		</script>
+
+		<p>You are being redirected to <a href="https://makzan.blog/feed/atom/">https://makzan.blog/feed/atom/</a></p>
+	</body>
+</html>
